87d5d931f4bfddafcdb1194c3e741875ae440238
lushlife99 <wjdcks282828@gmail.com>
2024-08-15 22:17:50 +0900

    feat: Add SceneListResponse record, Deserialization gpt reponse

---
+++ src/main/java/com/example/classicwave/dto/response/SceneListResponse.java
@@ -0,0 +1,16 @@
+package com.example.classicwave.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public record SceneListResponse(
+        @JsonProperty(required = true, value = "scene-list") List<SceneResponse> sceneResponseList
+        ) {
+    record SceneResponse(
+            @JsonProperty(required = true, value = "scene-description") String description,
+            @JsonProperty(required = true, value = "plot-summary") String plotSummary
+    ) {
+
+    }
+
+}
--- src/main/java/com/example/classicwave/service/CartoonCreationService.java
+++ src/main/java/com/example/classicwave/service/CartoonCreationService.java
@@ -1,7 +1,7 @@
 package com.example.classicwave.service;
 
-import com.example.classicwave.domain.Book;
 import com.example.classicwave.domain.Scene;
+import com.example.classicwave.dto.response.SceneListResponse;
 import com.example.classicwave.repository.BookRepository;
 import com.example.classicwave.repository.SceneRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +11,13 @@ import org.springframework.ai.chat.messages.UserMessage;
 
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.parser.BeanOutputParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,6 +25,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
@@ -28,27 +34,30 @@ public class CartoonCreationService {
 
     @Value("classpath:/prompts/scene-generation-system-message.st")
     private Resource sceneSystemPrompt;
+    @Value("classpath:/prompts/scene-generation-user-message.st")
+    private Resource sceneUserPrompt;
 
     private final BookRepository bookRepository;
     private final SceneRepository sceneRepository;
     private final OpenAiChatModel openAiChatModel;
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public List<Generation> getSceneListByBookInfo(String title, String isbnId) {
+    public SceneListResponse getSceneListByBookInfo(String title, String isbnId) {
         List<Scene> sceneList = new ArrayList<>();
-        Message userPrompt = getUserPrompt(title, isbnId);
+        BeanOutputConverter<SceneListResponse> outputConverter = new BeanOutputConverter<>(SceneListResponse.class);
+        PromptTemplate userPrompt = getUserPrompt(title, isbnId, outputConverter.getFormat());
         SystemPromptTemplate systemPrompt = new SystemPromptTemplate(sceneSystemPrompt);
-        Prompt prompt = new Prompt(List.of(userPrompt, systemPrompt.createMessage()),
+        Prompt prompt = new Prompt(List.of(userPrompt.createMessage(), systemPrompt.createMessage()),
                 OpenAiChatOptions.builder()
                         .withMaxTokens(4096)
                         .build());
-        List<Generation> response = openAiChatModel.call(prompt).getResults();
-        System.out.println(response);
-        return response;
+        Generation result = openAiChatModel.call(prompt).getResult();
+        SceneListResponse sceneListResponse = outputConverter.convert(result.getOutput().getContent());
+        return sceneListResponse;
     }
 
-    private Message getUserPrompt(String title, String isbnId) {
-        String message = "title : " + title + "\n" + "isbnId : " + isbnId;
-        return new UserMessage(message);
+    private PromptTemplate getUserPrompt(String title, String isbnId, String format) {
+        PromptTemplate promptTemplate = new PromptTemplate(sceneUserPrompt, Map.of("title", title, "isbnId", isbnId, "format", format));
+        return promptTemplate;
     }
 }

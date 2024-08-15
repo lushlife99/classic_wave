2c8fbb4ec910afd4b5dafeb68114de8b8124fc84
lushlife99 <wjdcks282828@gmail.com>
2024-08-15 20:50:52 +0900

    feat: Add CartoonCreationService, Controller, QuizPrompt

--- src/main/java/com/example/classicwave/controller/ClassicBookController.java
+++ src/main/java/com/example/classicwave/controller/ClassicBookController.java
@@ -1,10 +1,14 @@
 package com.example.classicwave.controller;
 
+import com.example.classicwave.service.CartoonCreationService;
 import com.example.classicwave.service.ClassicBookService;
 import com.example.classicwave.dto.request.EBookRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/classic-book")
@@ -12,10 +16,16 @@ import org.springframework.web.bind.annotation.*;
 public class ClassicBookController {
 
     private final ClassicBookService classicBookService;
+    private final CartoonCreationService cartoonCreationService;
 
     @PostMapping
     public ResponseEntity createClassicBook(@RequestBody EBookRequest bookRequest) {
         classicBookService.postToScheduler(bookRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/test")
+    public List<Generation> test(@RequestParam String title, @RequestParam String isbnId) {
+        return cartoonCreationService.getSceneListByBookInfo(title, isbnId);
     }
 }

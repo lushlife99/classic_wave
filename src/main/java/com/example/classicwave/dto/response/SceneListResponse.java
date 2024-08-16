7aece537a54ff9a9eab4c92ec0eb83769f287040
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-08-17 00:03:23 +0900

    Feat: Add SchedulerConfig.
    CreateCartoon scheduler, save file to S3 Storage

--- src/main/java/com/example/classicwave/dto/response/SceneListResponse.java
+++ src/main/java/com/example/classicwave/dto/response/SceneListResponse.java
@@ -6,7 +6,7 @@ import java.util.List;
 public record SceneListResponse(
         @JsonProperty(required = true, value = "scene-list") List<SceneResponse> sceneResponseList
         ) {
-    record SceneResponse(
+    public record SceneResponse(
             @JsonProperty(required = true, value = "scene-description") String description,
             @JsonProperty(required = true, value = "plot-summary") String plotSummary
     ) {
--- src/main/java/com/example/classicwave/service/CartoonCreationService.java
+++ src/main/java/com/example/classicwave/service/CartoonCreationService.java
@@ -1,5 +1,6 @@
 package com.example.classicwave.service;
 
+import com.example.classicwave.domain.Book;
 import com.example.classicwave.domain.Scene;
 import com.example.classicwave.dto.response.SceneListResponse;
 import com.example.classicwave.repository.BookRepository;
@@ -14,10 +15,22 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.image.ImageGeneration;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.Speech;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.ai.parser.BeanOutputParser;
+import org.springframework.ai.stabilityai.StabilityAiImageModel;
+import org.springframework.ai.stabilityai.StyleEnum;
+import org.springframework.ai.stabilityai.api.StabilityAiImageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -26,6 +39,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -39,13 +53,16 @@ public class CartoonCreationService {
 
     private final BookRepository bookRepository;
     private final SceneRepository sceneRepository;
+    private final OpenAiAudioSpeechModel audioSpeechModel;
+    private final StabilityAiImageModel imageModel;
     private final OpenAiChatModel openAiChatModel;
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public SceneListResponse getSceneListByBookInfo(String title, String isbnId) {
+    public SceneListResponse getSceneListByBookInfo(Book book) {
+
         List<Scene> sceneList = new ArrayList<>();
         BeanOutputConverter<SceneListResponse> outputConverter = new BeanOutputConverter<>(SceneListResponse.class);
-        PromptTemplate userPrompt = getUserPrompt(title, isbnId, outputConverter.getFormat());
+        PromptTemplate userPrompt = getUserPrompt(book.getName(), book.getIsbnId(), outputConverter.getFormat());
         SystemPromptTemplate systemPrompt = new SystemPromptTemplate(sceneSystemPrompt);
         Prompt prompt = new Prompt(List.of(userPrompt.createMessage(), systemPrompt.createMessage()),
                 OpenAiChatOptions.builder()
@@ -54,6 +71,50 @@ public class CartoonCreationService {
         Generation result = openAiChatModel.call(prompt).getResult();
         SceneListResponse sceneListResponse = outputConverter.convert(result.getOutput().getContent());
         return sceneListResponse;
+    }
+
+    public List<ImageGeneration> generateImages(SceneListResponse sceneListResponse) {
+        List<String> prompts = sceneListResponse.sceneResponseList().stream()
+                .map(SceneListResponse.SceneResponse::description)
+                .collect(Collectors.toList());
+
+        List<ImageGeneration> imageResults = new ArrayList<>();
+
+        for (String prompt : prompts) {
+            ImageResponse response = imageModel.call(
+                    new ImagePrompt(
+                            prompt,
+                            StabilityAiImageOptions.builder()
+                                    .withStylePreset(StyleEnum.COMIC_BOOK)
+                                    .build())
+            );
+            imageResults.add(response.getResult());
+        }
+
+        return imageResults;
+    }
+
+    public List<Speech> generateAudios(SceneListResponse sceneListResponse) {
+        List<String> prompts = sceneListResponse.sceneResponseList().stream()
+                .map(SceneListResponse.SceneResponse::plotSummary)
+                .collect(Collectors.toList());
+
+        List<Speech> speechResult = new ArrayList<>();
+
+        OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+                .withResponseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .withSpeed(1.0f)
+                .withModel(OpenAiAudioApi.TtsModel.TTS_1.value)
+                .build();
+
+        for (String prompt : prompts) {
+
+            SpeechPrompt speechPrompt = new SpeechPrompt(prompt, speechOptions);
+            SpeechResponse response = audioSpeechModel.call(speechPrompt);
+            speechResult.add(response.getResult());
+        }
+
+        return speechResult;
     }
 
     private PromptTemplate getUserPrompt(String title, String isbnId, String format) {

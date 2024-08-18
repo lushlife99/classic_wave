7c1b91c0bfc5a9812d03a5e72e9701fb054f6d04
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-19 00:04:39 +0900

    [Feat] : 퀴즈 생성 및 제출 기능 구현

--- src/main/java/com/example/classicwave/repository/BookRepository.java
+++ src/main/java/com/example/classicwave/repository/BookRepository.java
@@ -3,5 +3,8 @@ package com.example.classicwave.repository;
 import com.example.classicwave.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbnId(String isbnId);
 }

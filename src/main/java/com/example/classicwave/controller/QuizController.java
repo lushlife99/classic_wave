cb0f61d3e6b482e5f54dc71a1136965c030a6987
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-19 01:09:41 +0900

    [feat] : 퀴즈 생성 및 제출 스웨거 api 문서화

--- src/main/java/com/example/classicwave/controller/QuizController.java
+++ src/main/java/com/example/classicwave/controller/QuizController.java
@@ -5,6 +5,8 @@ import com.example.classicwave.dto.request.QuizSubmitRequest;
 import com.example.classicwave.dto.response.QuizListResponse;
 import com.example.classicwave.dto.response.QuizSubmitResponse;
 import com.example.classicwave.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@Tag(name = "퀴즈", description = "퀴즈 생성 및 제출 API")
 @Slf4j
 @RestController
 @RequestMapping("api/quiz")
@@ -22,8 +26,8 @@ public class QuizController {
 
     private final QuizService quizService;
 
-
     @PostMapping("/create")
+    @Operation(summary = "퀴즈 생성", description = "책 이름과 isbnid를 이용해 퀴즈를 생성")
     public ResponseEntity<QuizListResponse> createAndSaveQuiz(@RequestBody QuizRequest quizRequest) {
             String bookTitle = quizRequest.getBookTitle();
             String isbnId = quizRequest.getIsbnId();
@@ -33,8 +37,8 @@ public class QuizController {
 
     }
 
-
     @PostMapping("/submit")
+    @Operation(summary = "퀴즈 제출 및 채점", description = "사용자가 제출한 답을 바탕으로 답을 채점 후 점수를 반환")
     public ResponseEntity<QuizSubmitResponse> submitQuiz(@RequestBody QuizSubmitRequest quizSubmitRequest){
         Long quizListId = quizSubmitRequest.getQuizListId();
         List<Integer> userAnswers = quizSubmitRequest.getUserAnswers();

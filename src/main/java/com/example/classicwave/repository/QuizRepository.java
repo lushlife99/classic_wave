e91fdcf4282071acb3260ff772fd629ad590f137
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-19 00:04:39 +0900

    [Feat] : 퀴즈 생성 및 제출 기능 구현

--- src/main/java/com/example/classicwave/repository/QuizRepository.java
+++ src/main/java/com/example/classicwave/repository/QuizRepository.java
@@ -1,7 +1,11 @@
 package com.example.classicwave.repository;
 
 import com.example.classicwave.domain.Quiz;
+import com.example.classicwave.domain.QuizList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    List<Quiz> findByQuizList(QuizList quizList);
 }

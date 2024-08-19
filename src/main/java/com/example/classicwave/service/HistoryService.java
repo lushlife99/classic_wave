b85124b77d448ccde5d5fd5781cf33a456950156
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-19 21:45:05 +0900

    [Chore] : history setup

--- src/main/java/com/example/classicwave/service/HistoryService.java
+++ src/main/java/com/example/classicwave/service/HistoryService.java
@@ -1,9 +1,21 @@
 package com.example.classicwave.service;
 
+import com.example.classicwave.dto.response.QuizSubmitResponse;
+
+import java.util.List;
+
 public class HistoryService {
 
     /**
      * 책 썸네일 이름
      * 풀었던 퀴즈 내용, 점수..?
+     *
+     * 책 썸네일
+     * 발행년도 / 작가
+     * -> 누르면 풀었던 퀴즈 목록
      */
+
+    public List<QuizSubmitResponse> userHistory(Long userId){
+        return null;
+    }
 }

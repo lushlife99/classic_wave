60c760efbc272d4be1768cebb008ca04d43ab839
lushlife99 <101994803+lushlife99@users.noreply.github.com>
2024-08-20 22:17:52 +0900

    chore: api documentation

--- src/main/java/com/example/classicwave/controller/AuthController.java
+++ src/main/java/com/example/classicwave/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.example.classicwave.jwt.TokenInfo;
 import com.example.classicwave.service.AuthService;
 import com.example.classicwave.dto.request.AuthRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증", description = "유저 인증, 인가, 토큰재발급 API")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
@@ -23,14 +25,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/join")
-    @Operation(summary = "회원가입", description = "회원가입")
+    @Operation(summary = "회원가입", description = "유저 회원가입")
     public ResponseEntity join(@RequestBody AuthRequest authRequest) {
         authService.join(authRequest);
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    @Operation(summary = "로그인", description = "비밀번호 불일치, memberId가 존재하지 않을 시 오류반환. 성공 시 jwt 반환.")
+    @Operation(summary = "로그인", description = "id와 password로 로그인")
     public TokenInfo login(@RequestBody AuthRequest authRequest, HttpServletResponse response) {
         return authService.login(authRequest, response);
     }

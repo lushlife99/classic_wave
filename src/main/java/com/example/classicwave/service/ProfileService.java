b3272b03473a982d9e9c24a12c4a40d09b1a556b
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-21 15:29:47 +0900

    [Feat] : 유저 프로필 조회, 수정 기능 구현

---
+++ src/main/java/com/example/classicwave/service/ProfileService.java
@@ -0,0 +1,41 @@
+package com.example.classicwave.service;
+
+
+import com.example.classicwave.domain.Member;
+import com.example.classicwave.dto.domain.MemberDto;
+import com.example.classicwave.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public MemberDto getProfile(Long UserId){
+        Member member = memberRepository.findById(UserId)
+                .orElseThrow(()-> new RuntimeException("잘못된 요청 입니다."));
+
+        return new MemberDto(member.getUsername(),member.getIntroduction());
+    }
+
+    @Transactional
+    public MemberDto updateProfile(Long userId, String username, String introduction) {
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("잘못된 요청입니다."));
+
+        if (username != null && !username.isEmpty()) {
+            member.setLogInId(username);
+        }
+        if (introduction != null) {
+            member.setIntroduction(introduction);
+        }
+
+        memberRepository.save(member);
+
+        return new MemberDto(member.getUsername(), member.getIntroduction());
+    }
+}

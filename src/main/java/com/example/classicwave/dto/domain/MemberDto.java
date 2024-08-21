45d7d606755674bd3423327de903094e745af5a0
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-21 15:29:47 +0900

    [Feat] : 유저 프로필 조회, 수정 기능 구현

--- src/main/java/com/example/classicwave/dto/domain/MemberDto.java
+++ src/main/java/com/example/classicwave/dto/domain/MemberDto.java
@@ -1,9 +1,14 @@
 package com.example.classicwave.dto.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class MemberDto {
 
+    //private String pictureUrl;
+    private String name;
+    private String introduction;
 
 }

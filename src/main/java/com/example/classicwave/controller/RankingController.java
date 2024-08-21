e4738800506fe01d1f8c7c92119230e3410f6077
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-21 15:59:49 +0900

    [Feat] : 멤버 점수 필드 추가, 랭킹 기능 구현

--- src/main/java/com/example/classicwave/controller/RankingController.java
+++ src/main/java/com/example/classicwave/controller/RankingController.java
@@ -1,6 +1,7 @@
 package com.example.classicwave.controller;
 
 import com.example.classicwave.dto.domain.RankDto;
+import com.example.classicwave.service.RankService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @Tag(name = "랭킹", description = "사용자들의 랭킹 조회")
 @RequestMapping("api/ranking")
@@ -16,10 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RankingController {
 
+    private final RankService rankService;
+
     @GetMapping
     @Operation(summary = "랭킹 정보 반환", description = "현재 사용자 및 전체 사용자에 대한 랭킹 정보를 반환합니다.")
-    public ResponseEntity<RankDto> getRanking(){
-        return null;
+    public ResponseEntity<List<RankDto>> getRanking(){
+        List<RankDto> rankings = rankService.getRanking();
+        return ResponseEntity.ok(rankings);
     }
 }
 

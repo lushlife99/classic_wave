496d8bc4299637bde9df920086c8c276aa695831
lushlife99 <wjdcks282828@gmail.com>
2024-08-22 17:02:37 +0900

    chore: hikaripool config

--- src/main/java/com/example/classicwave/dto/response/SceneListResponse.java
+++ src/main/java/com/example/classicwave/dto/response/SceneListResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public record SceneListResponse(
         @JsonProperty(required = true, value = "copyright this book") boolean copyRight,
-        @JsonProperty(required = true, value = "total of 10 scene-list") List<SceneResponse> sceneResponseList,
+        @JsonProperty(required = true, value = "total of 10 scenes") List<SceneResponse> sceneResponseList,
         @JsonProperty(required = true, value = "book-title") String bookTitle,
         @JsonProperty(required = true, value = "book-author") String author,
         @JsonProperty(required = true, value = "publish-year") int pubYear

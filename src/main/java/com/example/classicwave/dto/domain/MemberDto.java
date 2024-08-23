3afb733452722d7b33ea6b6297d829988a58a1ab
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-08-24 07:18:46 +0900

    [Fix] : 사진 업로드 ㅠ

--- src/main/java/com/example/classicwave/dto/domain/MemberDto.java
+++ src/main/java/com/example/classicwave/dto/domain/MemberDto.java
@@ -2,13 +2,13 @@ package com.example.classicwave.dto.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.core.io.Resource;
 
 @Data
 @AllArgsConstructor
 public class MemberDto {
 
-    //private String pictureUrl;
     private String name;
     private String introduction;
-
+    private Resource profileImageUrl;
 }
